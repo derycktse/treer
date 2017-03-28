dd07eec24b1fb2796a2f5b414b8a5fdaf985e907
xiedangang <285205451@qq.com>
2017-03-29 00:20:36 +0800

    can draw directory tree now, but ugly

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 
 program
 	.version(package.version)
-	.option('-d, --directory [dir]', 'Please specify a directory to generate structure tree', '.')
+	.option('-d, --directory [dir]', 'Please specify a directory to generate structure tree', process.cwd())
 	.parse(process.argv);
 
 
@@ -14,36 +14,45 @@ console.log(program.directory)
 const dirToJson = (path) => {
 	let stats = fs.lstatSync(path),
 
-		structure = []
+		structure = {}
 
 	if (stats.isDirectory()) {
-
 		let dir = fs.readdirSync(path).map((child) => {
-			return dirToJson(path + '/' + child)
+			let childStats = fs.lstatSync(path + '/' + child)
+			return childStats.isDirectory() ? dirToJson(path + '/' + child) : child
 		})
 		let dirObj = {},
 			dirName = path.replace(/.*\//g, '')
-		dirObj[dirName] = dir
-		structure.push(dirObj)
+		structure[dirName] = dir
 	} else {
-		let fileName = path.replace(/.*\//g,'')
-		structure.push(fileName.toString())
+		let fileName = path.replace(/.*\//g, '')
+		return fileName
 	}
-	// console.log(structure)
 	return structure
 }
 
 const characters = ['|', '|--']
 
 const result = dirToJson(program.directory)
-// console.log(JSON.stringify(result))
-// console.log(result)
+	// console.log(JSON.stringify(result))
+	// console.log(result)
 
 
-const drawDirTree = (data)=>{
-	data.forEach((val,idx)=>{
-		console.log( typeof val + ` ` + idx)
-	})
+const drawDirTree = (data, placeholder) => {
+	for (let i in data) {
+		if (typeof data[i] === 'string') {
+			console.log(placeholder + data[i])
+		} else if (Array.isArray(data[i])) {
+			data[i].forEach((val) => {
+				if (typeof val === 'string') {
+					console.log(placeholder+"--" + val)
+				} else {
+					drawDirTree(val, placeholder.replace("--", "  ") + Array(i.length).join(" ") + "|--")
+
+				}
+			})
+		}
+	}
 }
 
-drawDirTree(result)
+drawDirTree(result, "|")
